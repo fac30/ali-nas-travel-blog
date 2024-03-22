8d2847110aa6a01784170ef20fef646a47379a90
Nas <n.choudhury1@live.co.uk>
2024-03-22 23:04:40 +0000

    changed port to 5096 due to conflict

---
+++ app.js
@@ -0,0 +1,50 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import expressLayout from "express-ejs-layouts";
+import methodOverride from "method-override";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+
+import connectDB from "./server/config/db.js";
+import mainRoutes from "./server/routers/main.js";
+import adminRoutes from "./server/routers/admin.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 5096;
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+app.use(express.static("uploads"));
+
+app.use(expressLayout);
+
+connectDB();
+
+app.set("layout", "./layouts/main");
+
+app.use("/", mainRoutes);
+app.use("/", adminRoutes);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
